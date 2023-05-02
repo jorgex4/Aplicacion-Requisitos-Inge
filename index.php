602cd91469488bf390199784b81314ab1990e633
Jorge Andrade <jorjeluisandrade300@gmail.com>
2023-05-02 14:56:56 -0500

    Mi primera aplicacion

---
+++ index.php
@@ -0,0 +1,28 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aplicacion</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <script src="js/bootstrap.min.js"> </script>
+</head>
+<body>
+    <form>
+  <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">Identificacion</label>
+    <input type="text" class="form-control" id="exampleInputid aria-describedby=">
+    
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label">Nombre</label>
+    <input type="text" class="form-control" id="">
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputDetail" class="form-label">Detalle</label>
+    <input type="text" class="form-control" id="">
+    
+  </div>
+  <button type="submit" class="btn btn-primary">Registrar</button>
+</form>
+</body>
